2caac58ec55f1841bb25c83c5ec27b1243f6a1b9
ingdariogiacomelli <ing.dariogiacomelli@gmail.com>
2025-09-05 18:05:51 +0200

    change events on input

--- ex_inputs.py
+++ ex_inputs.py
@@ -2,15 +2,19 @@ import runner, pyNaviGui as ng
 
 window = ng.Ng()
 (KEY_CLOSE, KEY_NAME, KEY_SURNAME, KEY_OPTIONS,KEY_BTN_READ_VALUES,KEY_SEX,KEY_MACHINE,
- KEY_BTN_FOCUS_TO_NAME, KEY_BTN_FOCUS_SURNAME,
+ KEY_BTN_FOCUS_TO_NAME, KEY_BTN_FOCUS_SURNAME,KEY_EMAIL,
  *_ ) = window.set_keys()
 
 (window.win_title('Test of inputs').win_size('800x600').move_to(30,80).
  text('Name').input('',   k=KEY_NAME, event_enter=True).br().
  text('Surname').input('', set_focus=True, event_tab=True , k=KEY_SURNAME).br().
+ text('Email').input('', set_focus=True, event_enter=True, event_tab=True, event_change=500, k=KEY_EMAIL).br().
+
  button('Read values',k=KEY_BTN_READ_VALUES).text('               ').
  button('Set focus on name',k=KEY_BTN_FOCUS_TO_NAME).button('Set focus on surname',k=KEY_BTN_FOCUS_SURNAME)
  )
+
+nr_changes = 0
 
 while True:
     event, values = window.read()
@@ -29,6 +33,17 @@ while True:
 
     if event == KEY_SURNAME + '_TAB':
         print('You pressed tab on the surname edit!')
+
+    if event == KEY_EMAIL :
+        print('You pressed enter on the email edit!')
+
+    if event == KEY_EMAIL + '_TAB':
+        print('You pressed tab on the email edit!')
+
+    if event == KEY_EMAIL + '_CHANGE':
+        nr_changes += 1
+        print(f'You change {nr_changes} times,  the email field!')
+
 
     if event == None:
         print('Window closed')
