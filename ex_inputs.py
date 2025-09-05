f0894039c1878e5d4cc51057cd73de47f299ad33
ingdariogiacomelli <ing.dariogiacomelli@gmail.com>
2025-09-05 17:59:29 +0200

    input with tab and enter, example

--- ex_inputs.py
+++ ex_inputs.py
@@ -6,8 +6,8 @@ window = ng.Ng()
  *_ ) = window.set_keys()
 
 (window.win_title('Test of inputs').win_size('800x600').move_to(30,80).
- text('Name').input('',   k=KEY_NAME).br().
- text('Surname').input('', set_focus=True,  k=KEY_SURNAME).br().
+ text('Name').input('',   k=KEY_NAME, event_enter=True).br().
+ text('Surname').input('', set_focus=True, event_tab=True , k=KEY_SURNAME).br().
  button('Read values',k=KEY_BTN_READ_VALUES).text('               ').
  button('Set focus on name',k=KEY_BTN_FOCUS_TO_NAME).button('Set focus on surname',k=KEY_BTN_FOCUS_SURNAME)
  )
@@ -23,6 +23,12 @@ while True:
 
     if event == KEY_BTN_FOCUS_SURNAME:
         window.set_focus(k=KEY_SURNAME)
+
+    if event == KEY_NAME:
+        print('You pressed anter on the name edit!')
+
+    if event == KEY_SURNAME + '_TAB':
+        print('You pressed tab on the surname edit!')
 
     if event == None:
         print('Window closed')
